93aaff908722d5024f9a8fd23a460bcd4460949a
peter <psvehla@hotmail.com>
2020-04-04 14:33:04 +1100

    This is where the tute leaves things. A bunch of plumbing is done, but the app doesn't really do anything yet.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
+// import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { selectSubreddit, fetchPostsIfNeeded } from './actions';
+import rootReducer from './reducers';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,  // lets us dispatch() functions
+    loggerMiddleware  // neat middleware that logs actions
+  )
+)
+
+store.dispatch(selectSubreddit('reactjs'));
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() => console.log(store.getState()));
 
 ReactDOM.render(
   <Provider store={store}>
